b76785283431ec3abb2cb718dbea1bc86a223831
Scadenet <95258942+Scadenet-com@users.noreply.github.com>
2024-06-06 15:55:47 -0600

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SwapWidget } from '@uniswap/widgets/dist/index.js';
+import '@uniswap/widgets/dist/fonts.css';
+
+function App() {
+  return (
+    <div className="Uniswap">
+      <SwapWidget />
+    </div>
+  );
+}
+
+export default App;
